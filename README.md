00b39fd93e870b1fc4bdfa15435aec1a2a26c721
bindi-v <bindi.vaghela@gmail.com>
2021-09-18 22:15:33 -0400

    add some changes to work password generator

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password-Generator
+
+It generate random password with 8 to 128 characters long.
+
+When user Click on Generate Password Button, it ask user for password length.
+
+If user add desired password length, it confirm that lenght with user.
+
+Next ask for add number to password. User select Ok than ask for add lowercase alphabet. Respectively going with Ok button asked user to add uppercase alphabet and then special character.
+
+If you select Cancel, it show user a Message that password must require number, lowercase, uppercase alphabet and special characters respectivly.
+
+After checking all characters, it generate random user desired length password.
+
+Then asked user if user wants to generate password again?
+
+User select Cancel than end function.
+
+User select Ok than cycle runs again for new desired password.
--- script.js
+++ script.js
@@ -1,45 +1,58 @@
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
+//  Write function 
 function writePassword() {
+
 // Generate function
   var generatePassword = function() {
-  
-  var number = [0,1,2,3,4,5,6,7,8,9];
-  var lowerCase = //"abcdefghijklmnopqrstuvwxyz";
-  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperCase = //"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var specialChar = ["!","@","#","$","%","^","&","*","_","+","-","=","|","?","(",")","{","}","[","]",":","<",">"];
+
+  // Array of options for computer to pick from
+  var number =   "0123456789";
+  var lowerCase =  "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChar =   "~!@#$%^&*()_+}{:?><|";
   var chosenChar = [];
   var password = [];
 
+  // Ask user for their choice of password length between 8 to 128 characters
   var userChoicePasswordLength
-   userChoicePasswordLength = window.prompt("Enter length of password from 8 to 128 ");
+   userChoicePasswordLength = window.prompt("Enter length of password from 8 to 128 "); 
   if (userChoicePasswordLength >= 8 && userChoicePasswordLength <= 128) {
-    window.confirm("You chose lenght of Password is " + userChoicePasswordLength);
-} else {
-  window.alert ("Thank You!");
-  return;
-}
+    window.alert("You chose lenght of Password is " + userChoicePasswordLength);
 
+    // If user pressed Cancel, function immediately End 
+  } else if(!userChoicePasswordLength){
+  window.alert ("Thank You!!");
+  return; 
+  }else {
+  // If user choose password length < 8 and > 128 than function alert message and end.
+  window.alert( "Password Length must be 8 to 128!");
+  return;
+  }
+
+// Ask user for to add Number in password 
   var userChoiceNumber = window.confirm("Add Number in Password ");
-  if (userChoiceNumber) {
+
+  // If user select Cancel function end
+  if (!userChoiceNumber) {
+    window.alert("Password must require Number!");
+      return;
+  } else {
     chosenChar += number;
     var index = Math.floor(Math.random() * number.length);
     password += number[index];
     window.alert("You chose " + userChoiceNumber);
-    
-  }else {
-   (!userChoiceNumber)
-    return "Password must require a number, special character, lowercase & uppercase alphabet! ";
-  }
+    }
 
-  var userChoiceLowerCase = window.confirm("Add Lower Case alphabet ");
+    // Ask user to add lowercase alphabet
+ var userChoiceLowerCase = window.confirm("Add LowerCase Alphabet! ");
+
+ // If user select Cancel, function end
   if (!userChoiceLowerCase) {
-    return; "Password must require a number, special character, lowercase & uppercase alphabet!  ";
+    window.alert("Password must require Lowercase Alphabet!  ");
+    return ;
   } else {
      chosenChar += lowerCase;
      index = Math.floor(Math.random() * lowerCase.length);
@@ -47,9 +60,13 @@ function writePassword() {
     window.alert("You chose " + userChoiceLowerCase);
   }
 
-  var userChoiceUpperCase = window.confirm("Add upper Case Alphabet ");
+  // Ask user to add uppercase alphabet
+ var userChoiceUpperCase = window.confirm("Add UpperCase Alphabet! ");
+
+ // If user select Cancel, function end 
   if (!userChoiceUpperCase) {
-    return;
+    window.alert("Password must require Uppercase Alphabet!");
+    return ;
   } else {
     chosenChar += upperCase;
     index = Math.floor(Math.random() * upperCase.length);
@@ -57,9 +74,14 @@ function writePassword() {
     window.alert("You Chose " + userChoiceUpperCase);
   }
 
-var userChoiceSpecialChar = window.confirm("Add Special Character ");
+
+  // Ask user to add special characters
+var userChoiceSpecialChar = window.confirm("Add Special Character! ");
+
+// If user select Cancel function will end
 if (!userChoiceSpecialChar) {
-  return "Password require special character!";
+  window.alert("Password must require Special Character!");
+  return ;
 } else {
   chosenChar += specialChar;
   index = Math.floor(Math.random() * specialChar.length);
@@ -67,26 +89,28 @@ if (!userChoiceSpecialChar) {
   window.alert("You Chose " + userChoiceSpecialChar);
 }
 
-
-for ( var i = 0; i < userChoicePasswordLength; i++) {
+// Get random characters from array of options based on password length
+for ( var i = 0; i < userChoicePasswordLength - 4 ; i++) {
   var randomNumber = Math.floor(Math.random() * chosenChar.length);
-  password += chosenChar.substring(randomNumber, randomNumber + 1);
+  password += chosenChar[randomNumber]; 
 } 
+
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
-window.alert("Your new password is " + password);
+
+// Print generated password
+window.alert("Hooray!! Your new password is :   " + password);
 console.log(password);
-// Ask user to generate password again
+
 var generatePasswordAgain = window.confirm("Generate Password Again?");
 
+// Ask user to generate password again
 if (generatePasswordAgain) {
  generatePassword();
-} 
+  }
 };
-// end generatePassword
 
+// Generate the password first time 
 generatePassword();
 
-
-
-}; //ends writePassword function
+};
